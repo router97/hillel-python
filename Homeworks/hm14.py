80b66d3a27a6f7804686678bbcbf155dd4a12db3
router <you@example.com>
2023-10-16 22:48:01 +0300

    pre-code

--- Homeworks/hm14.py
+++ Homeworks/hm14.py
@@ -2,4 +2,13 @@
 
 import os
 import shutil
-im
+import json
+
+""" Choosing task """
+while True:
+    task = input("enter 1 to write and print data, 2 for data types: ")
+    if task == '1' or task == '2':
+        break
+    else:
+        print('invalid input!')
+        continue

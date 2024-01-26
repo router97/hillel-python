0ebc9d5d00c77daec6f24efddf332397a987396b
router <you@example.com>
2024-01-26 23:47:42 +0300

    commit

--- C2/homeworks/hm4.py
+++ C2/homeworks/hm4.py
@@ -1,9 +1,27 @@
 # Homework 4 - Human Faker
 
-import faker
+from faker import Faker
+
+faker = Faker()
+
 
 class Human:
-    pass
+    
+    def __init__(self, 
+                 adress: str, 
+                 phone: str, 
+                 job: str,
+                 salary: float):
+        self.adress = adress
+        self.phone = phone
+        self.job = job
+        self.salary = salary
+    
+    def show_info(self):
+        result = ''
+        for i in [f"{key}: {value}\n" for key, value in self.__dict__.items()]:
+            result += i
+        print(result)
 
 class Man(Human):
     pass
@@ -12,4 +30,5 @@ class Woman(Human):
     pass
 
 if __name__ == '__main__':
-    pass
+    person1 = Human(faker.address(), faker.phone_number(), faker.job(), faker.random.randint(1000, 9999))
+    person1.show_info()

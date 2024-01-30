09278fa7e664dd850aab9f280f8c3d3638be7fe8
router <you@example.com>
2024-01-30 19:05:14 +0300

    commit

--- C2/homeworks/hm4.py
+++ C2/homeworks/hm4.py
@@ -1,34 +1,55 @@
 # Homework 4 - Human Faker
 
-from faker import Faker
-
-faker = Faker()
-
-
 class Human:
     
     def __init__(self, 
                  adress: str, 
                  phone: str, 
-                 job: str,
-                 salary: float):
+                 job: str, 
+                 salary: int):
         self.adress = adress
         self.phone = phone
         self.job = job
         self.salary = salary
     
     def show_info(self):
-        result = ''
-        for i in [f"{key}: {value}\n" for key, value in self.__dict__.items()]:
-            result += i
-        print(result)
+        info = '\n'.join([f"{key}: {value}" for key, value in self.__dict__.items()])
+        print(info)
 
 class Man(Human):
-    pass
+    
+    def __init__(self, 
+                 first_name: str, 
+                 last_name: str, 
+                 *args, 
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.first_name = first_name
+        self.last_name = last_name
 
 class Woman(Human):
-    pass
+    
+    def __init__(self, 
+                 first_name: str, 
+                 last_name: str, 
+                 *args, 
+                 **kwargs):
+        super().__init__(*args, **kwargs)
+        self.first_name = first_name
+        self.last_name = last_name
+
 
 if __name__ == '__main__':
-    person1 = Human(faker.address(), faker.phone_number(), faker.job(), faker.random.randint(1000, 9999))
-    person1.show_info()
+    
+    from faker import Faker
+    faker = Faker('uk_UA')
+    
+    human = Human(faker.address(), faker.phone_number(), faker.job(), faker.random.randint(1000, 9999))
+    man = Man(faker.first_name(), faker.last_name(), faker.address(), faker.phone_number(), faker.job(), faker.random.randint(1000, 9999))
+    woman = Woman(faker.first_name(), faker.last_name(), faker.address(), faker.phone_number(), faker.job(), faker.random.randint(1000, 9999))
+    
+    human.show_info()
+    print('-----')
+    man.show_info()
+    print('-----')
+    woman.show_info()
